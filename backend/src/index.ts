3b9b4f2d5a5eb60a8a00d631c0e75d5de55dce3d
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-09-08 16:25:51 +0100

    connect to db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,33 @@
 import app from './application';
 
-const port = process.env.PORT || 8000;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const db = require('knex')(({
+  client: 'mysql',
+  connection: {
+    host: process.env.MS_HOST,
+    user: process.env.MS_USER,
+    password: process.env.MS_PASS,
+    database: process.env.MS_DB
+  }
+}));
+
+app.get('/test', (_,res) => {
+  db('events')
+  .select('*')
+  .limit('3')
+  .then((data: any) => {
+    if (data.length === 0) {
+      res.status(404).send({'error': 'no data found'});
+    } else {
+      res.json(data);
+    }
+  })
+});
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
